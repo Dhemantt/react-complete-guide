e3e0cb52dcd0b3ca64956b19373974a63b0f7d25
Dhemantt <dixithemant02>
2022-10-10 18:03:29 +0530

    React Expense hard code

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,10 @@
+function Expenseitem() {
+    return (
+        <h2>Expense items!
+            <h3>food 10 rs
+            <h4>petrol 50 rs</h4></h3>,
+        </h2>
+        
+)}
+
+export default Expenseitem
