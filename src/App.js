76f4374d40e291e8fa7c0a114d829ded8a668c22
Dhemantt <dixithemant02>
2022-10-12 15:53:23 +0530

    created seperate component for amount details

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Expenseitem from "./components/Expenseitem";
 function App() {
   const Expenses = [
     { id:1, title: "petrol", amount: 100, date: new Date(2022, 10, 31) },
-    { id:2, title: "petrol", amount: 100, date: new Date(2022, 10, 30) },
-    { id:3, title: "petrol", amount: 100, date: new Date(2022, 10, 29) },
-    { id:4, title: "petrol", amount: 100, date: new Date(2022, 11, 31) },
+    { id:2, title: "car insurance", amount: 1000, date: new Date(2022, 10, 30) },
+    { id:3, title: "toilet paper", amount: 200, date: new Date(2022, 10, 29) },
+    { id:4, title: "grocery", amount: 500, date: new Date(2022, 11, 31) },
   ];
   return (
     <div>
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,14 +1,18 @@
+import ExpenseDetails from './ExpenseDetails';
+import ExpenseDate from './ExpenseDate';
 import './Expenseitem.css';
 
 function Expenseitem(props) {
-
+    
     return (
-        <div className='expense-items'>
-            <div>{props.Date.toISOString()}</div>
+        <div className='expense-item'>
+            <div>
+                <ExpenseDate date={props.date} />
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
-                <div className='expense-item__price'>{props.amount}</div>
             </div>
+            <ExpenseDetails amount={props.amount} />
         </div>
         
 )}
